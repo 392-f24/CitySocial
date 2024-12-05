138d95b85c8dde2b50e8f029a9628577c50340c4
Isabella Fishman <fishman.bellam@gmail.com>
2024-12-05 15:08:25 -0600

    Added questionnaire to Profile page - still needs styling and debugging possibly

--- src/components/Questionnaire.jsx
+++ src/components/Questionnaire.jsx
@@ -110,10 +110,6 @@ const Questionnaire = () => {
         );
     }
 
-    // Add this for debugging
-    const debugComplete = isQuestionnaireComplete();
-    console.log('Current answers:', answers);
-    console.log('Is complete:', debugComplete);
 
     return (
         <div className="max-w-2xl mx-auto p-6">
