6333fc09b60d2fabcb426546d78ca97a16f9be52
Isabella Fishman <fishman.bellam@gmail.com>
2024-12-05 23:18:34 -0600

    Changed Styling in Profiles

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from 'react';
 import { auth, db } from '../firebase';
 import { doc, getDoc, setDoc } from 'firebase/firestore';
 import { useNavigate } from 'react-router-dom';
+
 import SignOut from './auth/SignOut';
+
 import {questions} from '../data/questions';
 import RankingQuestion from './questionnaire/RankingQuestion';
 import InputListQuestion from './questionnaire/InputListQuestion';
@@ -79,55 +81,66 @@ const Profile = () => {
     }
 
     return (
-       <div>
-            <div>
-                <h1 className="text-5xl font-bold bg-gradient-to-r from-purple-600 to-pink-600 bg-clip-text text-transparent">
-                    Welcome, {userData?.name}
-                </h1>
-                <p className="mt-4 text-gray-600">Email: {userData?.email}</p>
+       <div className='px-4 sm:px-8 lg:px-16 py-10 sm:py-6'>
+            <h1 className="flex justify-center text-5xl lg:text-6xl sm:text-5xl font-bold bg-gradient-to-r from-purple-600 to-pink-600 bg-clip-text text-transparent">
+                Welcome, {userData?.name}
+            </h1>
+            <p className="flex justify-center mt-4 sm:text-base lg:text-lg text-black">Email: {userData?.email}</p>
+            
+
+
+            <div className='flex justify-center py-4'>
                 <SignOut />
-
-                <h2 className="text-2xl font-semibold text-gray-800 mt-6">Your Questionnaire Answers:</h2>
-
-                <div className="space-y-8 mt-6">
-                    {questions
-                        .filter((q) => ['rank', 'hobbies', 'text-multiple'].includes(q.type))
-                        .map((question) => (
-                            <div
-                                key={question.id}
-                                className="bg-white p-8 rounded-2xl shadow-lg border border-purple-100 hover:border-purple-200 transition-all duration-300"
-                            >
-                                <h2 className="text-xl font-semibold mb-3 text-gray-800">{question.question}</h2>
-                                <p className="text-gray-500 mb-6 text-sm">{question.description}</p>
-                                <div className="text-gray-700">
-                                    {question.type === 'rank' ? (
-                                        <RankingQuestion
-                                            question={question}
-                                            currentRanking={answers[question.id]}
-                                            onRankingChange={(newRanking) => handleAnswerUpdate(question.id, newRanking)}
-                                        />
-                                    ) : (
-                                        <InputListQuestion
-                                            type={question.type}
-                                            value={answers[question.id] || []}
-                                            onChange={(value) => handleAnswerUpdate(question.id, value)}
-                                        />
-                                    )}
-                                </div>
-                            </div>
-                        ))}
-                </div>
-
-
-                <button 
-                    className='bg-gradient-to-r from-purple-600 to-pink-600 text-white py-4 px-6 rounded-xl hover:shadow-lg hover:scale-105 transition-all duration-300 font-semibold text-base'
-                    onClick={handleSave}
-                >
-                        Save Changes
-                </button>
             </div>
+
+            
+
+            <h2 className="flex justify-center text-2xl sm:text-l font-semibold text-gray-800 mt-6"
+            >
+                Your Questionnaire Answers:
+            </h2>
+
+            <div className="space-y-6 lg:space-y-8 mt-6">
+                {questions
+                    .filter((q) => ['rank', 'hobbies', 'text-multiple'].includes(q.type))
+                    .map((question) => (
+                        <div
+                            key={question.id}
+                            className="bg-white p-8 rounded-2xl shadow-lg border border-purple-100 hover:border-purple-200 transition-all duration-300"
+                        >
+                            <h2 className="text-xl font-semibold mb-3 text-gray-800">{question.question}</h2>
+                            <p className="text-gray-500 mb-6 text-sm">{question.description}</p>
+                            <div className="text-gray-700">
+                                {question.type === 'rank' ? (
+                                    <RankingQuestion
+                                        question={question}
+                                        currentRanking={answers[question.id]}
+                                        onRankingChange={(newRanking) => handleAnswerUpdate(question.id, newRanking)}
+                                    />
+                                ) : (
+                                    <InputListQuestion
+                                        type={question.type}
+                                        value={answers[question.id] || []}
+                                        onChange={(value) => handleAnswerUpdate(question.id, value)}
+                                    />
+                                )}
+                            </div>
+                        </div>
+                    ))}
+            </div>
+
+
+        <div className='py-5 sm:py-4 lg:py-6 lg:px-3'>
+            <button 
+                className='sm:px-6 lg:py-5 lg:px-8 w-full bg-gradient-to-r from-purple-600 to-pink-600 text-white py-4 px-6 rounded-xl hover:shadow-lg hover:scale-105 transition-all duration-300 font-semibold text-base'
+                onClick={handleSave}
+            >
+                    Save Changes
+            </button>
         </div>
-    )
-}
+            
+        </div>
+    );
+};
 
 export default Profile;
